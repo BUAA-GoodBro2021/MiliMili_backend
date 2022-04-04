8abd2177c564ab0e92aa7002fec3ef29bd0d2ffc
暗中观察|.･｀) <2868470542@qq.com>
2022-04-04 21:25:22 +0800

    chore : finish first migrate

--- MiliMili/MiliMili/settings.py
+++ MiliMili/MiliMili/settings.py
@@ -152,9 +152,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'statics'),
-]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

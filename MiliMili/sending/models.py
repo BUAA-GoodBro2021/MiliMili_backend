366f4ce466cba2157273e9b5bc9885f45e62976d
暗中观察|.･｀) <2868470542@qq.com>
2022-04-07 22:40:19 +0800

    chore : add sending.py

--- MiliMili/sending/models.py
+++ MiliMili/sending/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Email_code(models.Model):
+    email_code = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'email_code'  # 改变当前模型类对应的表名
+        verbose_name = '验证码'
+        verbose_name_plural = verbose_name

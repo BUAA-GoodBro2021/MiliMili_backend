c7b7ec1c7859522a3d57aeb448904b154b6d6094
FL-Rover <2947653177@qq.com>
2022-04-14 19:49:17 +0800

    fix: correct the url in urls.py

--- MiliMili/user/urls.py
+++ MiliMili/user/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register),
-    path('login', views.login),
-    path('findPassword', views.findPassword),
-    path('upload-avatar', views.upload_avatar),
-    path('upload-file', views.upload_file)
+    path('register/', views.register),
+    path('login/', views.login),
+    path('findPassword/', views.findPassword),
+    path('upload-avatar/', views.upload_avatar),
+    path('upload-file/', views.upload_file)
 ]

1bf8d98960d24ad906111964f04cc77f640781bb
暗中观察|.･｀) <2868470542@qq.com>
2022-06-07 09:17:54 +0800

    fix : using Asia/Shanghai's time zone

--- MiliMili/MiliMili/settings.py
+++ MiliMili/MiliMili/settings.py
@@ -151,7 +151,7 @@ TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
